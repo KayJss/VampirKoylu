d70c3a8db23c6d93d1344f84045a0850a1f28e95
itsJustAbs <ardabaransaral6@gmail.com>
2024-06-01 12:59:16 +0300

    random role made

--- untitled4/lib/role_assing.dart
+++ untitled4/lib/role_assing.dart
@@ -6,11 +6,18 @@ void RoleAssing(){
 Map<String, roles> userRoles = {};
 
 var userList = Userdata.testusers();
+var roleList = RoleModel.fackRoles();
+Random random = Random();
 
 String getRandomName() {
- Random random = Random();
  return userList[random.nextInt(userList.length)].userId.toString();
 }
+
+String getRandomRoles(){
+ return roleList[random.nextInt(roleList.length)].RoleName;
+}
+
+
 int vampire = (userList.length / 4).round();
 
 for(int i = 0; i < vampire; i++){
@@ -20,5 +27,9 @@ userList.remove(RandomVampir);
 
 }
 
+
+String randomrole = getRandomRoles().toString();
+
 print(userRoles);
+print(randomrole);
 }
